61b41d61456e579c20ae19a84e2b43d0fede49bb
JagdishSuthar1 <jagdishsuthar4581@gmail.com>
2025-07-06 19:03:21 +0530

    added name and id in landing page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,12 @@
-import {prisma} from "@repo/db/client"
+import { prisma } from "@repo/db/client";
 
 export default async function Home() {
-const user = await prisma.user.findFirst();
+  const user = await prisma.user.findFirst();
 
-return (
-  <div>
-    {user?.id}
-    {user?.name}
-  </div>
-)
+  return (
+    <div>
+      <div>id :  {user?.id}</div>
+      <div>name : {user?.name}</div>
+    </div>
+  );
 }
